5d1e8ddf91356ea0121455cab16ec4c4ea6db414
javix64 <jav1.x64@gmail.com>
2021-02-23 11:51:43 +0100

    Added design part

--- src/components/Novedades.vue
+++ src/components/Novedades.vue
@@ -1,7 +1,18 @@
 <template lang="html">
 
   <section class="novedades">
+    <div v-for="i in content" v-bind:key="i.id">
+      <article v-if="i.novedad==true" class="producto">
+        <p class="novedad">¡Novedad!</p>
+        <h2>{{i.nombre}} </h2>
+        
+        <p>{{i.descripcion}} </p>
+        <img :src="i.photo">
+        <button> ¡Lo quiero!</button>
+        <p>Precio: <strong>${{i.precio}}</strong></p>
 
+      </article>
+    </div>
   </section>
 
 </template>
@@ -10,13 +21,13 @@
 
   export default  {
     name: 'novedades',
-    props: [],
+    props: ['shop'],
     mounted () {
 
     },
     data () {
       return {
-
+        content:this.shop
       }
     },
     methods: {
--- src/components/vMain.vue
+++ src/components/vMain.vue
@@ -1,7 +1,22 @@
 <template lang="html">
   <section class="main">
-      <Novedades />
-    {{shop}}
+    <section  v-if="!this.content">
+      <img :src="chargingSvg" class="svg">
+    </section>
+    <section v-if="this.content">
+      <Novedades v-bind:shop="shop"/>
+      <section class="categoria">
+    <div v-for="i in content" v-bind:key="i.id">
+      <article v-if="i.novedad==false" class="producto">
+        <h2>{{i.nombre}} </h2>
+        <p>{{i.descripcion}} </p>
+        <img :src="i.photo">
+        <button> ¡Lo quiero!</button>
+        <p>Precio: <strong>${{i.precio}}</strong></p>
+      </article>
+    </div>
+    </section>
+    </section>
   </section>
 
 </template>
@@ -12,14 +27,20 @@ import Novedades from '../components/Novedades.vue'
     name: 'main',
     props: ['shop'],
     mounted () {
-
+      this.timeout()
     },
     data () {
       return {
+        content:null,
+        chargingSvg:'animation.svg'
       }
     },
     methods: {
-
+      timeout(){
+        setTimeout(() => {
+          this.content=this.shop
+        }, 2000);
+      }
     },
     computed: {
 
